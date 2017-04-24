054a190db64783ef8580bcf1b283a3cddb2dab36
CEllis <tehwavy@gmail.com>
2017-04-24 15:58:37 +1000

    added get, post and delete request functionality with mongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,30 +10,34 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'buy flowers'}).save(function(err) {
-  if (err) throw err;
-  console.log('item saved');
-});
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+//var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
 
 app.get('/todo', function(req, res){
-  res.render('todo', {todos: data});
+  //get data from mongoDB and pass it to view
+  Todo.find({}, function(err, data) {
+    if (err) throw err;
+    res.render('todo', {todos: data});
+  });
 });
 
 app.post('/todo', urlencodedParser, function(req, res){
-  data.push(req.body);
-  res.json(data);
+  //get data from view and add to mongoDB
+  var newTodo = Todo(req.body).save(function(err, data) {
+    if (err) throw err;
+    res.json(data);
+  });
 });
 
 app.delete('/todo/:item', function(req, res){
-  data = data.filter(function(todo) {
-    return todo.item.replace(/ /g, '-') !== req.params.item;
+  //delete the requested item from mongoDB
+  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data) {
+    if (err) throw err;
+    res.json(data);
   });
-  res.json(data);
 });
 
 };
